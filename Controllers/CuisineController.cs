245f48ca25cb2609e61f8fab22e6343c38c396bf
Alkems <mynamezalter@gmail.com>
2022-10-05 14:55:37 +0300

    Add CuisineController, add LogAttribute, add Route to startupcs. Some errors.

---
+++ Controllers/CuisineController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Log]
+    public class CuisineController : Controller
+    {
+        public IActionResult Search(string name = "unknown")
+        {
+            var message = HtmlEncoder.Default.Encode(name);
+            throw new Exception("Something terrible has happened");
+
+            //return Content(message);
+            //return Content(message);
+            //return new EmptyResult();
+            //return File("/css/site.css", "text/css")
+            //return Json(HtmlEncoder.Default);
+            //return Redirect("https://www.tthk.ee");
+            //TODO Miks ei tööta? return RedirectToRoute("default", new { controller = "cuisine", action = "Second", count = 5 });
+            //TODO Miks ei tööta? return RedirectToRoute(nameof(Second), new { count = 5 });
+            //return RedirectToRoute("default", new { controller = "Home" }, Action = "About"});
+        }
+        public IActionResult Second(int count)
+        {
+            return Content($"Teine on siin! {count}");
+        }
+    }
+}
+
---
+++ Filters/LogAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            base.OnResultExecuting(context);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+        }
+    }
